f90043f8719550aa3d54a1c911ec0e573633677e
finedeer <finedeer@gmail.com>
2019-08-01 22:07:08 +0200

    constatns changes

--- js/constants.js
+++ js/constants.js
@@ -2,7 +2,7 @@
 (function () {
   window.constants = {
     URL: 'https://js.dump.academy/keksobooking',
-    DEBOUNCE_INTERVAL: 300,
+    DEBOUNCE_INTERVAL: 500,
     ERROR_RED_SHADOW: '0 0 5px 2px red'
   };
 })();
